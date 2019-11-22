ff8c6813acdf6d8d6d995ec2f4a3e8beba807dbe
Masato Hatayama <masato.hatayama@gmail.com>
2019-11-22 21:00:47 +0900

    submitted

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,10 +1,12 @@
 class Phrase
+  WORD_REGEX = /\b[\w']+\b/
+  
   def initialize(string)
     @string = string
   end
 
   def word_count
-    @string.downcase.split(/\W?[^\w']+\W?/).delete_if(&:empty?)
+    @string.downcase.scan(WORD_REGEX)
            .each_with_object(Hash.new(0)) { |w, h| h[w] += 1 }
   end
 end
