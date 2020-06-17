5af962b1b08a1dc971e133f9b452b67924df37fd
Masato Hayayama <masato.hatayama@gmail.com>
2020-06-18 04:26:08 +0900

    use delete method

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,6 +1,6 @@
 class PhoneNumber
   def self.clean(number)
-    cleaned = number.gsub(/\D/, "")
+    cleaned = number.delete("^0-9")
     cleaned = cleaned[1..10] if cleaned.length == 11 and cleaned[0] == "1"
     return cleaned if cleaned.length == 10 \
         and cleaned[0] =~ /[2-9]/ and cleaned[3] =~ /[2-9]/
