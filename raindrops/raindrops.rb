b1f700164d9966c772f2f4a9f29f3bcfbd8c4d95
Masato Hayayama <masato.hatayama@gmail.com>
2020-06-19 04:40:39 +0900

    fix

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,9 +1,12 @@
 class Raindrops
   def self.convert(num)
     output = ""
-    output += "Pling" if num % 3 == 0
-    output += "Plang" if num % 5 == 0
-    output += "Plong" if num % 7 == 0
+    %w"Pling Plang Plong".zip([3, 5, 7]).each{|sound, value|
+      output += sound if num % value == 0
+    }
+    # output += "Pling" if num % 3 == 0
+    # output += "Plang" if num % 5 == 0
+    # output += "Plong" if num % 7 == 0
     output.empty? ? num.to_s : output
   end
 end
