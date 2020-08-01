eaf5cba7137896fbb45b877f668c5061c8eff8cf
Masato Hatayama <masato.hatayama@gmail.com>
2020-08-01 11:59:47 +0900

    refactoring

--- saddle-points/saddle_points.rb
+++ saddle-points/saddle_points.rb
@@ -1,25 +1,15 @@
 class Matrix
+  attr_reader :rows, :columns
+
   def initialize(string)
-    @matrix = extract(string)
-  end
-
-  def rows
-    @matrix
-  end
-
-  def columns
-    @matrix.transpose
+    @rows = string.lines.map{|l| l.split().map(&:to_i)}
+    @columns = @rows.transpose
   end
 
   def saddle_points
-    all_points = (0...@matrix.size).to_a.product((0...@matrix[0].size).to_a)
+    all_points = (0...@rows.size).to_a.product((0...@columns.size).to_a)
     all_points.select do |i, j|
-      @matrix[i][j] == rows[i].max && @matrix[i][j] == columns[j].min
+      @rows[i][j] == @rows[i].max && @rows[i][j] == @columns[j].min
     end
-  end
-
-  private
-  def extract(string)
-    string.lines.map{|l| l.split().map(&:to_i)}
   end
 end
