28a287e7fb0340e7f06044e98f6d063e600219dc
Masato Hatayama <masato.hatayama@gmail.com>
2020-08-11 09:54:30 +0900

    submit

---
+++ pythagorean-triplet/pythagorean_triplet.rb
@@ -0,0 +1,37 @@
+class Triplet
+  def initialize(*nums)
+    raise ArgumentError unless nums.size == 3
+    @nums = nums
+  end
+
+  def sum
+    @nums.sum
+  end
+
+  def product
+    @nums.reduce(:*)
+  end
+
+  def pythagorean?
+    return false unless @nums[0] < @nums[1] && @nums[1] < @nums[2]
+    squares = @nums.sort.map{|n| n**2}
+    squares[0, 2].sum == squares[2]
+  end
+
+  def self.where(options)
+    raise ArgumentError unless options[:max_factor]
+    options[:min_factor] ||= 1
+
+    result = []
+    options[:min_factor].upto(options[:max_factor]) do |a|
+      (a + 1).upto(options[:max_factor]) do |b|
+        (b + 1).upto(options[:max_factor]) do |c|
+          triplet = new(a, b, c)
+          next if options[:sum] && options[:sum] != triplet.sum
+          result << triplet if triplet.pythagorean?
+        end
+      end
+    end
+    result
+  end
+end
