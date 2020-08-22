7f8861f833a53b1dcfbf03c327f3e8a91428124c
Masato Hatayama <masato.hatayama@gmail.com>
2020-08-23 08:46:06 +0900

    rubocop

--- collatz-conjecture/collatz_conjecture.rb
+++ collatz-conjecture/collatz_conjecture.rb
@@ -2,6 +2,7 @@ class CollatzConjecture
   def self.steps(num)
     raise ArgumentError unless num.positive?
     return 0 if num == 1
-    self.steps(num.even? ? num / 2 : num * 3 + 1) + 1
+
+    steps(num.even? ? num / 2 : num * 3 + 1) + 1
   end
 end
