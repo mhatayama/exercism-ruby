d03ee57bd20399fa30e95d25d8657961299d99eb
Masato Hatayama <masato.hatayama@gmail.com>
2020-08-30 08:30:08 +0900

    refactoring

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,28 +1,29 @@
 class Clock
-  MINUTES_PER_HOUR = 60
-  HOURS_PER_DAY = 24
-  MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY
-
   def initialize(hour: 0, minute: 0)
     @minutes = (hour * MINUTES_PER_HOUR + minute) % MINUTES_PER_DAY
   end
 
   def to_s
-    sprintf("%02d:%02d", @minutes / MINUTES_PER_HOUR, @minutes % MINUTES_PER_HOUR)
+    '%02d:%02d'.format([@minutes / MINUTES_PER_HOUR, @minutes % MINUTES_PER_HOUR])
   end
 
-  def +(other_clock)
-    Clock.new(minute: @minutes + other_clock.minutes)
+  def +(other)
+    Clock.new(minute: @minutes + other.minutes)
   end
 
-  def -(other_clock)
-    Clock.new(minute: @minutes - other_clock.minutes)
+  def -(other)
+    Clock.new(minute: @minutes - other.minutes)
   end
 
-  def ==(other_clock)
-    @minutes == other_clock.minutes
+  def ==(other)
+    @minutes == other.minutes
   end
 
   protected
+
   attr_reader :minutes
+
+  MINUTES_PER_HOUR = 60
+  HOURS_PER_DAY = 24
+  MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY
 end
